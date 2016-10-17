8eddf8b901b442c7ac18a2b4e09f8f2b864f6a71
Shtkddud123 <sangyoung123@googlemail.com>
2016-10-17 15:18:09 +0100

    Added Monte-Carlo pi algorithm

--- direct_pi.py
+++ direct_pi.py
@@ -17,6 +17,22 @@ class SM_alg():
             if (i*i) + (j*j) < 1:
                 self.Nhits = self.Nhits + 1
                 
-a = SM_alg(4000)
-a.direct_sampling()
-print a.Nhits 
+    def markov_pi(self,x_markov,y_markov,delta):
+        """Pi comes out as four times the ratio of hits
+        to trials 
+        """
+        self.x_markov = x_markov
+        self.y_markov = y_markov
+        self.delta = delta
+
+        deltax = np.random.uniform(-self.delta, self.delta, self.N)
+        deltay = np.random.uniform(-self.delta, self.delta, self.N)
+
+        for i,j in np.nditer([deltax,deltay]):            
+            if np.absolute(self.x_markov + i) < 1 and np.absolute(self.y_markov + j) < 1:
+
+                self.x_markov = self.x_markov + i
+                self.y_markov = self.y_markov + j                
+
+                if (self.x_markov * self.x_markov) + (self.y_markov * self.y_markov) < 1:
+                    self.Nhits = self.Nhits + 1 
